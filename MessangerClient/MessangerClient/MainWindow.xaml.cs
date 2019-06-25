2b027f4030459e236e92ed191e92ff7b91f3b9d8
Alisher Kalabaev <alisher.alish333@gmail.com>
2019-06-25 21:40:23 +0600

    Yeeeeeeah Boyyyyy

---
+++ MessangerClient/MessangerClient/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace MessangerClient
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TcpClient client;
+        string clientName;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            connectDisconnectButton.Tag = false;
+            ipTextBox.Text = "10.1.4.87";
+            portTextBox.Text = "12345";
+            nameTextBox.Text = "AlishBEk";
+        }
+
+        private void ConnectDisconnectButtonClick(object sender, RoutedEventArgs e)
+        {
+            if(!(bool)connectDisconnectButton.Tag)
+            {
+                clientName = nameTextBox.Text;
+                client = new TcpClient();
+                client.BeginConnect(IPAddress.Parse(ipTextBox.Text), int.Parse(portTextBox.Text), ClientWork, client);
+
+                connectDisconnectButton.Tag = true;
+                connectDisconnectButton.Content = "Disconnect";
+            }
+            else
+            {
+                client.Client.Shutdown(SocketShutdown.Both);
+                client.Close();
+
+                connectDisconnectButton.Tag = false;
+                connectDisconnectButton.Content = "Connect";
+            }
+        }
+
+        private void ClientWork(object state)
+        {
+            TcpClient client = (state as IAsyncResult).AsyncState as TcpClient;
+
+            if (!string.IsNullOrEmpty(clientName))
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    client.Client.Send(Encoding.UTF8.GetBytes(nameTextBox.Text));
+                });
+
+                ThreadPool.QueueUserWorkItem(ClientListen, client);
+
+                MessageBox.Show("Connected");
+            }
+            else
+            {
+                MessageBox.Show("Напишите свое имя");
+
+                client.Client.Shutdown(SocketShutdown.Both);
+                client.Close();
+
+                Dispatcher.Invoke(() =>
+                {
+                    connectDisconnectButton.Tag = false;
+                    connectDisconnectButton.Content = "Connect";
+                });
+            }
+        }
+
+        private void ClientListen(object state)
+        {
+            TcpClient client = state as TcpClient;
+            byte[] buf = new byte[4 * 1024];
+            int recSize;
+
+            try
+            {
+                while (client.Connected)
+                {
+                    recSize = client.Client.Receive(buf);
+                    Dispatcher.Invoke(() =>
+                    {
+                        messageTextBox.Text += Encoding.UTF8.GetString(buf, 0, recSize) + "\n";
+                    });
+                }
+            }
+            catch (NullReferenceException) { }
+            catch (SocketException) { }
+        }
+
+        private void SendButtonClick(object sender, RoutedEventArgs e)
+        {
+            if ((bool)connectDisconnectButton.Tag)
+            {
+                client.Client.Send(Encoding.UTF8.GetBytes(sendMessageTextBox.Text));
+                sendMessageTextBox.Text = string.Empty;
+            }
+        }
+    }
+}
---
+++ MessangerServer/MessangerServer/MainWindow.xaml.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace MessangerServer
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Thread serverThread;
+        TcpListener server;
+
+        List<TcpClient> clients = new List<TcpClient>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            startStopButton.Tag = false;
+
+            ipsComboBox.Items.Add("0.0.0.0");
+            ipsComboBox.Items.Add("127.0.0.1");
+
+            foreach(IPAddress ip in Dns.GetHostEntry(Dns.GetHostName()).AddressList)
+            {
+                ipsComboBox.Items.Add(ip.ToString());
+            }
+
+            ipsComboBox.SelectedIndex = 0;
+        }
+
+        delegate void DelegateAppendTextBox(TextBox tb, string str);
+
+        private void AppendTextBox(TextBox tb, string str)
+        {
+            tb.Text += str + "\n";
+        }
+
+        private void PortTextBoxKeyDown(object sender, KeyEventArgs e)
+        {
+            if( (e.Key >= Key.D0 && e.Key <= Key.D9) || (e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9) )
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void StartStopButtonClick(object sender, RoutedEventArgs e)
+        {
+            if(!(bool)startStopButton.Tag)
+            {
+                startStopButton.Content = "Stop";
+                startStopButton.Tag = true;
+
+                server = new TcpListener(IPAddress.Parse(ipsComboBox.SelectedItem.ToString()), int.Parse(portTextBox.Text));
+                server.Start(100);
+
+                serverThread = new Thread(ServerThreadRoutine);
+                serverThread.IsBackground = true;
+                serverThread.Start(server);
+
+                logTextBox.Text += "Server have started his work\n";
+            }
+            else
+            {
+                startStopButton.Content = "Start";
+                startStopButton.Tag = false;
+
+                server.Stop();
+                clients.Clear();
+            }
+        }
+
+        private void ServerThreadRoutine(object state)
+        {
+            TcpListener server = state as TcpListener;
+            byte[] buf = new byte[4 * 1024];
+            int recSize;
+
+            try
+            {
+                while (true)
+                {
+                    TcpClient client = server.AcceptTcpClient();
+
+
+                    recSize = client.Client.Receive(buf);
+                    client.Client.Send(Encoding.UTF8.GetBytes("Hello " + Encoding.UTF8.GetString(buf, 0, recSize) + "\n"));
+                    clients.Add(client);
+
+                    Dispatcher.Invoke(new DelegateAppendTextBox(AppendTextBox), new object[] {logTextBox, $"Client {client.Client.LocalEndPoint} added\n" });
+
+                    ThreadPool.QueueUserWorkItem(ClientThreadRoutine, client);
+                }
+            }
+            catch(InvalidOperationException)
+            {
+
+            }
+            catch(SocketException)
+            {
+
+            }
+        }
+
+        private void ClientThreadRoutine(object state)
+        {
+            TcpClient client = state as TcpClient;
+            byte[] buf = new byte[4 * 1024];
+            int recSize;
+
+            while(client.Connected)
+            {
+                try
+                {
+                    recSize = client.Client.Receive(buf);
+
+                    Dispatcher.Invoke(new DelegateAppendTextBox(AppendTextBox), new object[] { logTextBox, $"Client {client.Client.LocalEndPoint} wrote {Encoding.UTF8.GetString(buf, 0, recSize)}\n" });
+
+                    foreach (TcpClient sendClient in clients)
+                    {
+                        sendClient.Client.Send(Encoding.UTF8.GetBytes(Encoding.UTF8.GetString(buf, 0, recSize) + "\n"));
+                    }
+                }
+                catch(SocketException)
+                {
+
+                }
+            }
+
+            clients.Remove(client);
+            client.Client.Shutdown(SocketShutdown.Both);
+            client.Close();
+        }
+    }
+}
