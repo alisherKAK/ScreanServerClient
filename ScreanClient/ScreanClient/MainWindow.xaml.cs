621f06e786b4d6db19ba56a475c2b68764ce4ded
Alisher Kalabaev <alisher.alish333@gmail.com>
2019-06-26 21:33:40 +0600

    screen server

---
+++ ScreanClient/ScreanClient/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScreanClient
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<byte> imageBytes = new List<byte>();
+        Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GetSreenButtonClick(object sender, RoutedEventArgs e)
+        {
+            IPAddress serverIp;
+            byte[] buf = new byte[64 * 1024];
+            int recSize;
+
+            if (IPAddress.TryParse(serverIpTextBox.Text, out serverIp))
+            {
+                EndPoint endPoint = new IPEndPoint(serverIp, int.Parse(serverPortTextBox.Text));
+                client.SendTo(Encoding.UTF8.GetBytes("get sreen"), endPoint);
+                recSize = client.ReceiveFrom(buf, ref endPoint);
+                imageBytes.AddRange(buf);
+            }
+        }
+    }
+}
---
+++ ScreanServer/ScreanServer/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Threading;
+using System.Net.Sockets;
+
+namespace ScreanServer
+{
+    class Program
+    {
+        static Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        static Thread serverThread;
+
+        static void Main(string[] args)
+        {
+            server.Bind(new IPEndPoint(IPAddress.Any, 12345));
+            serverThread = new Thread(ListenClients);
+            serverThread.IsBackground = true;
+            serverThread.Start(server);
+
+            Console.ReadLine();
+        }
+
+        private static void ListenClients(object state)
+        {
+            Socket server = state as Socket;
+            EndPoint clientEndPoint = new IPEndPoint(0, 0);
+            byte[] buf = new byte[64 * 1024];
+            int recSize;
+
+            while (true)
+            {
+                recSize = server.ReceiveFrom(buf, ref clientEndPoint);
+
+                if (Encoding.UTF8.GetString(buf, 0, recSize).ToLower() == "get sreen")
+                {
+                    using (Bitmap BM = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height))
+                    using (Graphics GH = Graphics.FromImage(BM as Image))
+                    {
+                        var bytes = server.SendTo(ImageToByte(BM), clientEndPoint);
+                        GH.CopyFromScreen(0, 0, 0, 0, BM.Size);
+                    }
+                }
+            }
+        }
+
+        private static byte[] ImageToByte(Image img)
+        {
+            ImageConverter converter = new ImageConverter();
+            return (byte[])converter.ConvertTo(img, typeof(byte[]));
+        }
+    }
+}
